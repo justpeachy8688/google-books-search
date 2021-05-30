d3d0018301ab65a6146c7a3f7694c705887f9578
Taylor Sanders <tsanders3515@gmail.com>
2021-05-30 15:05:15 -0800

    push for LA

---
+++ client/src/utils/API.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default {
+    //gets all books
+    getBooks: function() {
+        return axios.get("./api/books");
+    },
+//deletes book with given id
+    deleteBook: function() {
+        return axios.delete("/api/books/" + id);
+    },
+
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    }
+}
